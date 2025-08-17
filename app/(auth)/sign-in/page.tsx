9a38926a999809b7082f2b6e5ac163df211f1255
ralphbrodriguez <ralphbrodriguez@gmail.com>
2025-08-17 13:39:09 -0700

    Project build md updated

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Home = () => {
+const SignIn = () => {
   return (
-    <div>Home</div>
+    <div>SignIn</div>
   )
 }
 
-export default Home
+export default SignIn
