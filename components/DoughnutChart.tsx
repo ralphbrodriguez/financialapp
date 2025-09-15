3a6b54b3df4aba15088a01fe0855aaf77532c328
ralphbrodriguez <ralphbrodriguez@gmail.com>
2025-09-15 12:46:45 -0700

    Fixed doughnut

--- components/DoughnutChart.tsx
+++ components/DoughnutChart.tsx
@@ -15,8 +15,7 @@ const DoughnutChart = ({ accounts }: DoughnutChartProps) => {
     datasets: [
       {
         label: 'Banks',
-        // data: balances,
-        data: [1250, 2500, 3750],
+        data: balances,
         backgroundColor: ['#0747b6', '#2265d8', '#2f91fa'] 
       }
     ],
